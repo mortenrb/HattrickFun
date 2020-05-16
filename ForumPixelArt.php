102c4b7d71cd86d8c93b8ee178f997d41f63d6c3
Mortenrb <mortenrb@live.no>
2020-05-16 23:57:49 +0200

    Created this wonderful file
    Created a file that takes an input of 1s and 0s and generates pixel art that is compatible with the HT forum.
    Tried to optimalize it to a decent degree with colspan, and these optimalizations made me go from -3712 characters on the forum post to +903 (or 7612 total characters to 2997 total characters) on the snoopy pixelart.

---
+++ ForumPixelArt.php
@@ -0,0 +1,120 @@
+<?php
+$art = array();
+
+/* Pikachu */
+/*
+$art[] = [0,0,0,0,0,0,0,1,0,0,0,0,0];
+$art[] = [1,1,1,0,0,0,1,0,1,0,1,1,1];
+$art[] = [1,1,0,1,0,0,1,0,0,1,0,1,1];
+$art[] = [0,1,0,0,1,1,1,1,1,0,0,1,0];
+$art[] = [0,0,1,0,0,0,0,0,0,0,1,0,0];
+$art[] = [0,0,1,0,1,0,0,0,1,0,1,0,0];
+$art[] = [0,0,1,0,0,0,1,0,0,0,1,0,0];
+$art[] = [0,1,0,1,0,1,1,1,0,1,0,1,0];
+$art[] = [0,1,0,0,0,0,0,0,0,0,0,1,0];
+$art[] = [0,1,0,0,1,0,0,0,1,0,0,1,0];
+$art[] = [0,1,0,1,1,0,0,0,1,1,0,1,0];
+$art[] = [0,1,0,0,0,0,0,0,0,0,0,1,0];
+$art[] = [0,0,1,0,0,1,1,1,0,0,1,0,0];
+$art[] = [0,0,0,1,1,0,0,0,1,1,0,0,0];
+*/
+
+/* Snoopy */
+/*
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0];
+$art[] = [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0];
+$art[] = [0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1];
+$art[] = [0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,1];
+$art[] = [0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,0,1];
+$art[] = [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,1];
+$art[] = [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,1];
+$art[] = [0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1];
+$art[] = [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,1];
+$art[] = [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0];
+$art[] = [0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0];
+$art[] = [0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,0,1,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,0,1,0,1,1,0,1,0,1,0,0,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,0,1,0,1,1,0,0,0,1,0,0,1,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0];
+$art[] = [0,0,0,0,0,1,0,1,0,0,0,0,1,1,1,1,1,0,1,0,0,0,0,0];
+$art[] = [0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0];
+$art[] = [0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0];
+$art[] = [0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0];
+$art[] = [0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0];
+*/
+
+/* Just some array variables */
+$previous = null; //Tells the script which pixel was the previous one
+$length = 1; //The length of each pixelrow
+$temprow = array(); //Poor naming(?), but temporarily holding each "artrow"
+$newarray = array(); //Another poor naming, it's the art array that we'll be working on when generating the table
+
+foreach($art as $a) {
+	/* Make Pixelgroups, so that we can use colspan */
+	foreach($a as $pixel) {
+		/* If the previous pixel is the same as current pixel, we add to the colspan length */
+		if($previous == $pixel) {
+			$length++;
+		} 
+		/* Else we put the previous pixed plus its length in a temporary array for the row */
+		else {
+			$temprow[] = [$previous, $length];
+			$length = 1;
+		}
+		$previous = $pixel;
+	}
+	/* Hack for the last row */
+	$temprow[] = [$previous, $length];
+	
+	/* Hey, blanks are useless, so let's remove them! 
+	 * Saved some couple of characters, but might not be needed all the time
+	 */
+	if($previous == 0)
+		array_pop($temprow);
+	
+	/* Reset the pixelgroup settings */
+	$length = 1;
+	$previous = null;
+	$newarray[] = $temprow;
+	$temprow = array();
+}
+
+/* Initialize table */
+echo "[table]<br />";
+foreach($newarray as $rows) {
+	echo "[tr]";
+	foreach($rows as $pxgrp) {
+		/* Check if the pixedgroup is larger than 2, as:
+		 * [th][/th] = 9 chars, while
+		 *  colspan=X = 10+ chars
+		 * So colspan is only saving space at 3+ lengths
+		 */
+		if($pxgrp[1] > 2) {
+			if($pxgrp[0] == 1)
+				echo "[th colspan=".$pxgrp[1]."][/th]";
+			else
+				echo "[td colspan=".$pxgrp[1]."][/td]";
+		} else {
+			for($i=1;$i<=$pxgrp[1];$i++)
+				if($pxgrp[0] == 1)
+				echo "[th][/th]";
+			else
+				echo "[td][/td]";
+		}
+		
+	}
+	echo "[/tr]<br />";
+}
+/* End table! */
+echo "[/table]";
